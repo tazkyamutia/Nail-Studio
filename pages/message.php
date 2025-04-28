c966ebeeaef48059a9e139d4102f1aac0e102c93
tazkyamutia <tazkyamr34@example.com>
2025-04-29 00:18:15 +0700

    p

---
+++ pages/message.php
@@ -0,0 +1,37 @@
+<?php include '../views/header.php'; ?>
+  <link rel="stylesheet" href="../Tazkya-HTML/css/style2.css">
+		<main>
+			<div class="head-title">
+				<div class="left">
+					<h1>Dashboard</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li><i class='bx bx-chevron-right' ></i></li>
+						<li>
+							<a class="active" href="#">Messages</a>
+						</li>
+					</ul>
+				</div>
+				
+			</div>
+
+            <div class="chat-container">
+				<div class="chat-header">
+					<h2>Messages</h2>
+				</div>
+				<div class="toggle">
+					<span id="toggle-user">Switch to Receiver</span>
+				</div>
+				<div class="chat-messages" id="chat-messages"></div>
+				<div class="chat-input">
+					<input type="text" id="message-input" placeholder="Type your message here...">
+					<button id="send-button">Send</button>
+				</div>
+			</div>
+        
+    </section>
+	
+<?php include '../views/footer.php'; ?>
+	
