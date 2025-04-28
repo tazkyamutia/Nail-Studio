f69a806d6279463aadff70db9117f73d5f78f798
tazkyamutia <tazkyamr34@example.com>
2025-04-29 00:58:22 +0700

    p

--- pages/product.php
+++ pages/product.php
@@ -1,4 +1,5 @@
 <?php include '../views/header.php'; ?>
+<?php include '../views/sidebar.php'; ?>
 	<link rel="stylesheet" href="../Tazkya-HTML/css/style2.css">
         <main>
 			<div class="head-title">
