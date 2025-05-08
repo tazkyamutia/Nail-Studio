4ba8a5866396da1dfccb0de7fb614d11621069be
Raynaldi Paniroean Panjaitan <raynaldypand.20@gmail.com>
2025-05-08 11:51:57 +0700

    ganti header.php > index.php

--- pages/login.php
+++ pages/login.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 if ($_SESSION["role"] == "admin") {                         
                     header("Location: dashboard.php");                     
                 } else if ($_SESSION["role"] == "member") {                         
-                    header("Location: header.php");                     
+                    header("Location: index.php");                     
                 }                     
                 exit;                 
             } else {                     
